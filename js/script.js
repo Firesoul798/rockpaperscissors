d4eeb165ee740e0c4788cfc8ef58132324562afa
Rahmel DeSoto <greysoul798@gmail.com>
2018-11-14 23:07:50 +0000

    if

--- js/script.js
+++ js/script.js
@@ -4,10 +4,23 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
+var userChoice="";
+var computerChoice="";
+var winner="";
 
 
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function(){
-    $("#userChoice").text($('#input').val());
+    var userChoice = $("#input").val();
+    $("#userChoice").text(userChoice);
+    var randomNumber=Math.random(); 
+    console.log(randomNumber)
+    if (randomNumber < .333333333){
+        computerChoice= "Rock";
+    }else if (randomNumber < .6666666){
+        computerChoice="Paper";
+    }else if (randomNumber < .99999999){
+        computerChoice="Scissors";
+    }
+    $("#computerChoice").text(computerChoice);
 });
